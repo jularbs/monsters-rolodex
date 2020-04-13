9994a0ff8bfb31353e182f4315deacdcb4ad0ddd
Ralph Jularbal <ralphjularbal@yahoo.com>
2020-04-13 23:33:39 +0800

    Updates

---
+++ precache-manifest.46bfc4324a4cf5ace4cfb877900947d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6b8c23b3eab457ab8d5970176d758ef",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8f701088a59daadd6118",
+    "url": "/monsters-rolodex/static/css/main.f4b72184.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f701088a59daadd6118",
+    "url": "/monsters-rolodex/static/js/main.cbd9bba7.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
